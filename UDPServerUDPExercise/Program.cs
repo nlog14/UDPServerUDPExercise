e881f7c3e85de796de4f4a33a311e27af7879312
Nohely <ngedeon14@yahoo.com>
2021-11-09 12:16:10 +0100

    Add project files.

---
+++ UDPServerUDPExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPServerUDPExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UDP SERVER");
+            StartListener();
+        }
+
+        public static void StartListener()
+        {
+            using (UdpClient socket = new UdpClient())
+            {
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, 10100));
+
+                while (true)
+                {
+                    IPEndPoint from = null;
+                    byte[] data = socket.Receive(ref from);
+                    string recieved = Encoding.UTF8.GetString(data);
+                    Console.WriteLine("Server receieved: " + recieved);
+                    Console.WriteLine("Client address: " + from.Address);
+
+                    socket.Send(data, data.Length, from);
+                }
+            }
+        }
+    }
+}
